36ee1b430dc727370e3fb76f37831e273415584e
Ali Yasin Emir <aliyasin.emir16@gmail.com>
2023-06-01 16:55:36 +0300

    I've impemented projects section and sound feature.

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,50 +33,39 @@ navLink.forEach((n) => n.addEventListener('click', linkAction));
 
 /*=============== Change Color Style =============== */
 
-let root = document.querySelector(':root');
-let gcs = getComputedStyle(root);
-let hue = gcs.getPropertyValue('--hue');
+const root = document.querySelector(':root');
+const gcs = getComputedStyle(root);
+const hue = gcs.getPropertyValue('--hue');
 const colorPalette = document.querySelector('.nav__colors');
+const homeTitle = document.querySelector('.home__title');
+/* Adding color mode sound */
+const colorSound = new Audio('/assets/sound/color.mp3');
+const defaultColorSound = new Audio('/assets/sound/default-color.mp3');
+
+console.log(hue);
 
 colorPalette.addEventListener('click', () => {
-  const root = document.querySelector(':root');
-  const gcs = getComputedStyle(root);
-  const hue = gcs.getPropertyValue('--hue');
-  let homeTitle = document.querySelector('.home__title');
-
-  switch (hue) {
-    default:
-      root.style.setProperty('--hue', '');
-      root.style.setProperty('--first-color', '');
-      root.style.setProperty('--container-color', '');
-      root.style.setProperty('--first-color-alt', '');
-      homeTitle.style.setProperty('color', '');
-  }
+  defaultColorSound.play();
+  root.style.removeProperty('--hue');
+  root.style.removeProperty('--first-color');
+  root.style.removeProperty('--container-color');
+  root.style.removeProperty('--first-color-alt');
+  homeTitle.style.removeProperty('color');
 });
 
 // BBL
 
+/* Change number color */
+
 const bbl = document.querySelector('.bbl');
 
 bbl.addEventListener('click', () => {
-  let root = document.querySelector(':root');
-  let gcs = getComputedStyle(root);
-  let hue = gcs.getPropertyValue('--hue');
-  let homeTitle = document.querySelector('.home__title');
-
-  if ((hue = 162)) {
-    root.style.setProperty('--hue', '250');
-    root.style.setProperty('--first-color', 'hsl(250, 100%, 75%)');
-    root.style.setProperty('--first-color-alt', 'hsl(267, 51%, 59%)');
-    homeTitle.style.setProperty('color', 'yellow');
-    root.style.setProperty('--container-color', 'hsl(267, 47%, 24%');
-  } else {
-    root.style.setProperty('--hue', '');
-    root.style.setProperty('--first-color', '');
-    root.style.setProperty('--container-color', '');
-    root.style.setProperty('--first-color-alt', '');
-    homeTitle.style.setProperty('color', '');
-  }
+  colorSound.play();
+  root.style.setProperty('--hue', '250');
+  root.style.setProperty('--first-color', 'hsl(250, 100%, 75%)');
+  root.style.setProperty('--first-color-alt', 'hsl(267, 51%, 59%)');
+  homeTitle.style.setProperty('color', 'yellow');
+  root.style.setProperty('--container-color', 'hsl(267, 47%, 24%');
 });
 
 // FF
@@ -84,24 +73,12 @@ bbl.addEventListener('click', () => {
 const ff = document.querySelector('.ff');
 
 ff.addEventListener('click', () => {
-  let root = document.querySelector(':root');
-  let gcs = getComputedStyle(root);
-  let hue = gcs.getPropertyValue('--hue');
-  let homeTitle = document.querySelector('.home__title');
-
-  if ((hue = 162)) {
-    root.style.setProperty('--hue', '193');
-    root.style.setProperty('--first-color', 'hsla(193, 82%, 52%, 1)');
-    root.style.setProperty('--first-color-alt', 'hsla(192, 63%, 41%, 1)');
-    homeTitle.style.setProperty('color', 'hsla(193, 82%, 52%, 1)');
-    root.style.setProperty('--container-color', 'hsla(196, 85%, 32%, 1)');
-  } else {
-    root.style.setProperty('--hue', '');
-    root.style.setProperty('--first-color', '');
-    root.style.setProperty('--container-color', '');
-    root.style.setProperty('--first-color-alt', '');
-    homeTitle.style.setProperty('color', '');
-  }
+  colorSound.play();
+  root.style.setProperty('--hue', '193');
+  root.style.setProperty('--first-color', 'hsla(193, 82%, 52%, 1)');
+  root.style.setProperty('--first-color-alt', 'hsla(192, 63%, 41%, 1)');
+  homeTitle.style.setProperty('color', 'hsla(193, 82%, 52%, 1)');
+  root.style.setProperty('--container-color', 'hsla(196, 85%, 32%, 1)');
 });
 
 /* Change container color */
@@ -129,6 +106,15 @@ colorPalette.addEventListener('click', () => {
 /////////////////////////
 
 /*=============== ADD BLUR TO HEADER ===============*/
+
+// pageYOffset = scrollY, but it's better to use pageYOffset because of the browsercompatibility
+
+window.addEventListener('scroll', () => {
+  console.log(window.pageYOffset);
+  if (window.scrollY >= 50) {
+    console.log('header is blur now');
+  }
+});
 
 const blurHeader = () => {
   const header = document.getElementById('header');
